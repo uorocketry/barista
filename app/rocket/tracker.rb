c3253147704777ddb07071856ad5d97fb8ad74bd
NikhilPeri <nikhil.peri@shopify.com>
2017-11-18 15:07:43 -0500

    initial

---
+++ app/rocket/tracker.rb
@@ -0,0 +1,23 @@
+class Rocket::Tracker
+  def initialize(gyro, accelerometer)
+    @velocity = { x: 0.0, y: 0.0, z: 0.0 }
+    @position = { x: 0.0, y: 0.0, z: 0.0 }
+    @
+  end
+
+  def compute_velocity
+
+  end
+
+  def compute_position
+
+  end
+
+  def acceletation
+
+  end
+
+  def heading
+
+  end
+end
