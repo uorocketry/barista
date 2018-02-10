6fc93553efca0ee8e644ad8427ed733254f36b71
NikhilPeri <nikhil.peri@shopify.com>
2018-02-10 17:37:39 -0500

    servo and pyro

---
+++ app/utils/servo.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+
+FREQUENCY = 100 # Hz
+MINIMUM_DUTY_CYCLE = FREQUENCY * 0.001
+MAXIMUM_DUTY_CYCLE = FREQUENCY/5
+
+class Servo(object):
+    def __init__(self, pin):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(pin, FREQUENCY)
+        self.pwm.start(MINIMUM_DUTY_CYCLE)
+
+
+    def write(self, position):
+        duty_cycle = MINIMUM_DUTY_CYCLE + (float(position)/180)*(MAXIMUM_DUTY_CYCLE-MINIMUM_DUTY_CYCLE)
+        self.pwm.ChangeDutyCycle(duty_cycle)
