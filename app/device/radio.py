be8f0989b96293e286ef35d5233adb1c541952de
buzuloiu <runner183@hotmail.com>
2018-03-11 18:22:07 -0400

    initial radio

--- app/device/radio.py
+++ app/device/radio.py
@@ -1,6 +1,34 @@
+import serial
+from time import sleep
+import logging
+import json
+
 class Radio(object):
-    def receive(self):
-        return None
-        # { action : None, data: None }
-    def transmit(self, message):
-        pass
+    def __init__(self,baud=9600,port='/dev/ttyUSB0'):
+        try:
+            self.ser = serial.Serial(port,baud,timeout=None)
+            logging.info("Radio Initialized")
+        except Exception as e:
+            logging.error('error:{}'.format(e))
+
+    def write(self, raw_data):
+        logging.info(json.dumps(raw_data))
+        try:
+            return self.ser.write(json.dumps(raw_data)+'\n')
+        except Exception as e:
+            logging.error('error: {}, data: {}'.format(e, raw_data))
+
+    def read(self):
+        try:
+            return self.ser.readline()
+        except Exception as e:
+            logging.error('error: {}'.format(e))
+            return "ERROR"
+
+    def sleep(self):
+        #TODO: make it sleep
+        return
+
+    def wake(self):
+        #TODO: make it wake up from sleep
+        return
