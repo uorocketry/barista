812a33589565fb79fc71ea8822e9d4dacc09c9f5
mginoya <ginoya.manit@gmail.com>
2018-03-28 21:30:01 -0400

    changed some configurations values for servo utility

--- app/utils/servo.py
+++ app/utils/servo.py
@@ -3,15 +3,15 @@ if os.environ.has_key('ROCKET_PRODUCTION'):
     import RPi.GPIO as GPIO
 
 FREQUENCY = 100 # Hz
-MINIMUM_DUTY_CYCLE = FREQUENCY * 0.001
-MAXIMUM_DUTY_CYCLE = FREQUENCY/5
+MINIMUM_DUTY_CYCLE = FREQUENCY * (float(3383)/32400)
+MAXIMUM_DUTY_CYCLE = FREQUENCY * (float(1099)/4500)
 
 class Servo(object):
     def __init__(self, pin):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin, GPIO.OUT)
         self.pwm = GPIO.PWM(pin, FREQUENCY)
-        self.pwm.start(MINIMUM_DUTY_CYCLE)
+        self.pwm.start((MINIMUM_DUTY_CYCLE+MAXIMUM_DUTY_CYCLE)/2)
 
 
     def write(self, position):
