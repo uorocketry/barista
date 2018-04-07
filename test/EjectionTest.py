9e1e42d13c4aba19e3e247e99821e87c45c80ce0
Anthony Lin <anthonyzqlin@gmail.com>
2018-04-07 16:20:44 -0400

    lol

--- test/EjectionTest.py
+++ test/EjectionTest.py
@@ -1,40 +1,61 @@
 import time
 from app.device.radio import Radio
-from app.rocket.device_factory import DeviceFactory
 
-device_factory = DeviceFactory()
+
 radio = Radio()
 
-while(True):
-    message = radio.receive()
-    if message['action'] == u'confirm_comms':
-        radio.transmit('reply', 'Radios receiving and transmitting.')
+def wait_loop():
+    while True:
+        print('Waiting')
+        message = radio.receive()
+        if message == None:
+            time.sleep(1)
+        else:
+            return message
 
-    elif message['action'] == u'test_ejection_stage_one':
-        radio.transmit('reply', 'Testing Stage One of ejection.')
-        radio.transmit('reply', 'Deploying Stage One in T-minus:')
-        for x in range(5):
-            t = str(5-x)
-            radio.transmit('reply', x)
-            sleep(1)
-        radio.tansmit('reply', '0')
-        device_factory.parachute.deploy_stage_one()
-        radio.transmit('reply', 'Stage One deployed')
+def transmit(action, data):
+    print('Transmiting: ' + str(action) +' - '+ str(data))
+    radio.transmit(action, data)
 
 
-    elif message['action'] == u'test_ejection_stage_two':
-        radio.transmit('reply', 'Testing stage two of ejection.')
-        radio.transmit('reply', 'Deploying Stage Two in T-minus:')
-        for x in range(5):
-            t = str(5-x)
-            radio.transmit('reply', time)
-            sleep(1)
-        radio.transmit('reply', '0')
-        device_factory.parachute.deploy_stage_two()
-        radio.transmit('reply', 'Stage Two deployed.')
+if __name__ == "__main__":
+    wait_loop()
+    transmit('Hello', 'from Pi')
 
-    elif message == None:
-        sleep(1)
+    # message = wait_loop()
+    # if message['action'] == u'deploy':
+    #     print('deploy')
 
-    elif message['action'] == 'quit':
-        break;
+    while True:
+        message = wait_loop()
+
+        if message['action'] == u'test_ejection_stage_one':
+            transmit('reply', 'Testing Stage One of ejection.')
+            radio.transmit('reply', 'Deploying Stage One in T-minus:')
+            for x in range(5):
+                t = str(5-x)
+                radio.transmit('reply', t)
+                time.sleep(1)
+            radio.transmit('reply', '0')
+            # device_factory.parachute.deploy_stage_one()
+            transmit('reply', 'Stage One deployed')
+
+
+        elif message['action'] == u'test_ejection_stage_two':
+            transmit('reply', 'Testing stage two of ejection.')
+            radio.transmit('reply', 'Deploying Stage Two in T-minus:')
+            for x in range(5):
+                t = str(5-x)
+                radio.transmit('reply', t)
+                time.sleep(1)
+            radio.transmit('reply', '0')
+            # device_factory.parachute.deploy_stage_two()
+            transmit('reply', 'Stage Two deployed.')
+
+        elif message == None:
+            time.sleep(1)
+
+        elif message['action'] == u'quit':
+            break;
+
+        time.sleep(1)
