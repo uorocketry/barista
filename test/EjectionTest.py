85aa84cd2b8f253f8c5f7b357e1b2b4528eb0926
Paul Buzuloiu <pbuzuloiu@yahoo.ca>
2018-04-12 08:24:04 -0400

    Parachute Test

--- test/EjectionTest.py
+++ test/EjectionTest.py
@@ -1,8 +1,10 @@
 import time
 from app.device.radio import Radio
+from app.device.parachute import Parachute
 
 
 radio = Radio()
+parachute = Parachute()
 
 def wait_loop():
     while True:
@@ -29,7 +31,13 @@ if __name__ == "__main__":
     while True:
         message = wait_loop()
 
-        if message['action'] == u'test_ejection_stage_one':
+
+        if message['action'] == u'ping':
+            transmit('reply', 'PI!')
+
+
+
+        elif message['action'] == u'test_ejection_stage_one':
             transmit('reply', 'Testing Stage One of ejection.')
             radio.transmit('reply', 'Deploying Stage One in T-minus:')
             for x in range(5):
@@ -37,8 +45,11 @@ if __name__ == "__main__":
                 radio.transmit('reply', t)
                 time.sleep(1)
             radio.transmit('reply', '0')
+            parachute.deploy_stage_one()
             # device_factory.parachute.deploy_stage_one()
             transmit('reply', 'Stage One deployed')
+            time.sleep(15)
+            parachute.cut_power()
 
 
         elif message['action'] == u'test_ejection_stage_two':
@@ -49,8 +60,11 @@ if __name__ == "__main__":
                 radio.transmit('reply', t)
                 time.sleep(1)
             radio.transmit('reply', '0')
+            parachute.deploy_stage_two()
             # device_factory.parachute.deploy_stage_two()
             transmit('reply', 'Stage Two deployed.')
+            time.sleep(15)
+            parachute.cut_power()
 
         elif message == None:
             time.sleep(1)
