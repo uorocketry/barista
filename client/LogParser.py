258563231b1412f3a196ff3aa7ff471126ffe359
anthony-talevi <anthony.talevi@gmail.com>
2018-05-15 09:13:12 -0400

    Finished Log parser (except for errors and warnings)

--- client/LogParser.py
+++ client/LogParser.py
@@ -23,50 +23,29 @@ class LogParser(object):
     def getWarnings(self,log_file):
         print("To implement")
 
-    def getXs(self,log_file, param):
+    def getData(self, log_file, plane, param):
         param_pattern = '(?<=('+param+' )).*'
-        value_pattern = r'(?<=x: )(-?\d*\.?\d*)'
+        value_pattern = r'(?<=' + plane + r': )(-?\d*\.?\d*)'
 
         parse_file = open(log_file, 'r')
 
-        x_data = []
+        data = []
         for line in parse_file:
             match = re.search(param_pattern, line)
             if(match):
-                x = re.search(value_pattern, match.group()).group()
+                d = re.search(value_pattern, match.group()).group()
                 time = self.getTime(line)
-                x_data.append([x, time])
-        return pd.DataFrame(x_data, columns=['X '+param, 'Time'])
+                data.append([time, d])
+        df = pd.DataFrame(data, columns=['Time', plane + ' ' + param])
+        df = df.set_index('Time')
+        df = df.astype(float)
+        return df
+
+    def getXs(self,log_file, param):
+        return self.getData(log_file, 'x', param)
 
     def getYs(self,log_file, param):
-        param_pattern = '(?<=('+param+' )).*'
-        value_pattern = r'(?<=y: )(-?\d*\.?\d*)'
-
-        parse_file = open(log_file, 'r')
-
-        y_data = []
-        for line in parse_file:
-                match = re.search(param_pattern, line)
-                if(match):
-                    time = self.getTime(line)
-                    y = re.search(value_pattern, match.group()).group()
-                    y_data.append((time, y))
-
-        return pd.DataFrame(y_data, columns=["Time", 'Y ' + param])
+        return self.getData(log_file, 'y', param)
 
     def getZs(self,log_file, param):
-        param_pattern = '(?<=('+param+' )).*'
-        value_pattern = r'(?<=z: )(-?\d*\.?\d*)'
-
-        parse_file = open(log_file, 'r')
-
-        z_data = []
-        for line in parse_file:
-            time = self.getTime(line)
-            match = re.search(param_pattern, line)
-            if(match):
-                z = re.search(value_pattern, match.group()).group()
-                z_data.append((time, z))
-
-
-        return pd.DataFrame(z_data, columns=['Time', 'Z ' + param])
+        return self.getData(log_file, 'z', param)
