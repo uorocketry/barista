8d1dc5969da5e3da80e331f7a81cbbe9d1e96ec6
azqlin <anthonyzqlin@gmail.com>
2018-05-18 20:30:16 -0400

    nice for this

--- app/device/altimeter.py
+++ app/device/altimeter.py
@@ -17,20 +17,14 @@ class Altimeter(object):
     def __init__(self):
         self.bus = I2C(1, MPL3115A2_ADDRESS)
 
-        self.bus.write_byte(CTRL_REG1, 0xB8) #OSR = 128 to set to altimeter TODO: delete later
-
+        self.bus.write_byte(CTRL_REG1, 0xB8) #OSR = 128 to set to altimeter
         self.bus.write_byte(PT_DATA_CFG, 0x07)
-
         self.bus.write_byte(CTRL_REG1, 0xB9) #activate polling
-        offset_height = 70 ## Ottawa altitude: 70 m, Las Cruses altitude: 1,189 m
-        offset_height = np.binary_repr(-offset_height, width = 8)
-        offset_height = int(offset_height, 2)
-        self.bus.write_byte(OFF_H, offset_height)
 
-        status = self.read_byte(STATUS_REG)
-
+        status = self.bus.read_byte(STATUS_REG)
         while (status & 0x08) == False:
-            self.read_byte(STATUS_REG)
+            status = self.bus.read_byte(STATUS_REG)
+            sleep(0.5)
 
     def read_altitude(self):
         raw_data = self.bus.read_block(OUT_P_MSB, 3)
@@ -42,21 +36,21 @@ class Altimeter(object):
 
 
 ##//MAYBE
-    def read_bar_setting(self):
-        setting = self.bus.read_block(BAR_IN_MSB, 2)
-        try:
-            return parse_raw_data(setting)*2
-        except Exception as e:
-            logging.error('error: %s, raw_data: %s', e, raw_data)
-            return(-1)
-
-    #Parameter is bar setting/2
-    def write_bar_setting(self, input):
-        if input < 0 or input > 131071:
-            raise ValueError('Input out of acceptable bounds.')
-        else:
-            self.bus.write_block(BAR_IN_MSB, int(input))
-            setting = read_bar_setting()
+    # def read_bar_setting(self):
+    #     setting = self.bus.read_block(BAR_IN_MSB, 2)
+    #     try:
+    #         return parse_raw_data(setting)*2
+    #     except Exception as e:
+    #         logging.error('error: %s, raw_data: %s', e, raw_data)
+    #         return(-1)
+    #
+    # #Parameter is bar setting/2
+    # def write_bar_setting(self, input):
+    #     if input < 0 or input > 131071:
+    #         raise ValueError('Input out of acceptable bounds.')
+    #     else:
+    #         self.bus.write_block(BAR_IN_MSB, int(input))
+    #         setting = read_bar_setting()
 ##MAYBE
 
     @staticmethod
@@ -66,23 +60,37 @@ class Altimeter(object):
         str_csb = '{0:08b}'.format(raw_data[1])
         str_lsb = '{0:08b}'.format(raw_data[2])
 
-        parsed_data = str_msb + str_csb + str_lsb[3] + str_lsb[2] + str_lsb[1] + str_lsb[0]
+        parsed_data = str_msb + str_csb
+        parsed_data_fractions = str_lsb[4] + str_lsb[5] + str_lsb[6] + str_lsb[7]
 
         parsed_altitude = int(parsed_data, 2)
-        parsed_altitude = np.binary_repr(-parsed_altitude, width = 20)
-        parsed_altitude = int(parsed_altitude, 2)
+        if str_msb[0] == '1':
+            parsed_altitude = np.binary_repr(-1*parsed_altitude, width = 15)
+            parsed_altitude = int(parsed_data, 2)
 
-        return parsed_altitude
+        parsed_altitude_fractions = int(parsed_data_fractions, 2)/16
+        if str_lsb[4] == '1':
+            parsed_altitude_fractions = -1*parsed_altitude_fractions
+
+        final_altitude = parsed_altitude + parsed_altitude_fractions
+        return final_altitude
 
     def parse_raw_data_temp(raw_data):
 
         str_msb = '{0:08b}'.format(raw_data[0])
         str_lsb = '{0:08b}'.format(raw_data[1])
 
-        parsed_data = str_msb + str_lsb[3] + str_lsb[2] + str_lsb[1] + str_lsb[0]
+        parsed_data = str_msb
+        parsed_data_fractions = str_lsb[4] + str_lsb[5] + str_lsb[6] + str_lsb[7]
 
         parsed_temp = int(parsed_data, 2)
-        parsed_temp = np.binary_repr(-parsed_temp, width = 16)
-        parsed_temp = int(parsed_temp, 2)
+        if str_msb[0] == '1':
+            parsed_altitude = np.binary_repr(-1*parsed_altitude, width = 13)
+            parsed_altitude = int(parsed_data, 2)
 
-        return parsed_temp
+        parsed_temp_fractions = int(parsed_temp_fractions, 2)/16
+        if str_lsb[4] == '1':
+            parsed_temp_fractions =  -1*parsed_temp_fractions
+
+        final_temp = parsed_temp + parsed_temp_fractions
+        return final_temp
