9bdf9ca9fa65f273264b1eedd290d46eca43d6bf
azqlin <anthonyzqlin@gmail.com>
2018-06-09 17:06:40 -0400

    99% working

--- app/device/altimeter.py
+++ app/device/altimeter.py
@@ -30,11 +30,11 @@ class Altimeter(object):
 
     def read_altitude(self):
         raw_data = self.bus.read_block(OUT_P_MSB, 3)
-        return Altimeter.parse_raw_data_altitude(raw_data)
+        return Altimeter.parse_raw_data(raw_data)
 
     def read_temp(self):
         raw_data = self.bus.read_block(OUT_T_MSB, 2)
-        return Altimeter.parse_raw_data_temp(raw_data)
+        return Altimeter.parse_raw_data(raw_data)
 
     def reset_bar_input(self): ## TODO: Temp code
         self.bus.write_byte(0x14, 0xC5)
@@ -44,7 +44,7 @@ class Altimeter(object):
     # def read_bar_setting(self):
     #     setting = self.bus.read_block(BAR_IN_MSB, 2)
     #     try:
-    #         return parse_raw_data(setting)*2
+    #         return parse_raw_data(setting, False)*2
     #     except Exception as e:
     #         logging.error('error: %s, raw_data: %s', e, raw_data)
     #         return(-1)
@@ -55,65 +55,48 @@ class Altimeter(object):
         if input < 0 or input > 131071: ## TODO: change value for New Mexico
             raise ValueError('Input out of acceptable bounds.')
         else:
-            equiv_pressure = 101950 # Ottawa
+            equiv_pressure = 101950/2 # Ottawa
             equiv_pressure = np.binary_repr(equiv_pressure, width = 16)
             equiv_pressure = equiv_pressure[-16:]
-            equiv_pressure_msb = equiv_pressure[0:8]
-            equiv_pressure_lsb = equiv_pressure[8:]
+            equiv_pressure_msb = int(equiv_pressure[0:8], 2)
+            equiv_pressure_lsb = int(equiv_pressure[8:], 2)
 
-            self.bus.write_block(BAR_IN_MSB, equiv_pressure_msb)
-            self.bus.write_block(BAR_IN_LSB, equiv_pressure_lsb)
+            self.bus.write_byte(BAR_IN_MSB, equiv_pressure_msb)
+            self.bus.write_byte(BAR_IN_LSB, equiv_pressure_lsb)
             # read_bar_setting()
 ## //
 
     @staticmethod
-    def parse_raw_data_altitude(raw_data):
+    def parse_raw_data(raw_data, twos_comp = True):
+        if len(raw_data) == 3:
+            str_msb = '{0:08b}'.format(raw_data[0])
+            str_csb = '{0:08b}'.format(raw_data[1])
+            str_lsb = '{0:08b}'.format(raw_data[2])
 
-        str_msb = '{0:08b}'.format(raw_data[0])
-        str_csb = '{0:08b}'.format(raw_data[1])
-        str_lsb = '{0:08b}'.format(raw_data[2])
+            parsed_data = str_msb + str_csb
+            str_length = 16
+        else:
+            str_msb = '{0:08b}'.format(raw_data[0])
+            str_lsb = '{0:08b}'.format(raw_data[1])
 
-        parsed_data = str_msb + str_csb
-        parsed_data_fractions = str_lsb[0] + str_lsb[1] + str_lsb[2] + str_lsb[3]
+            parsed_data = str_msb
+            str_length = 8
 
-        parsed_altitude = int(parsed_data, 2)
-        parsed_altitude_fractions = int(parsed_data_fractions, 2)
+        data_fractions = str_lsb[0] + str_lsb[1] + str_lsb[2] + str_lsb[3]
 
-        if str_msb[0] == '1':
-            parsed_altitude = np.binary_repr(-parsed_altitude, width = 16)
-            parsed_altitude = parsed_altitude[-16:]
-            parsed_altitude = -1*int(parsed_altitude, 2)
+        data = int(parsed_data, 2)
+        data_fractions = int(data_fractions, 2)
 
-            parsed_altitude_fractions = np.binary_repr(-parsed_altitude_fractions, width = 4)
-            parsed_altitude_fractions = parsed_altitude_fractions[-4:]
-            parsed_altitude_fractions = -1*int(parsed_altitude_fractions, 2)
+        if (str_msb[0] == '1' and twos_comp == True):
+            data = np.binary_repr(-data, width = str_length)
+            data = data[-str_length:]
+            data = -1*int(data, 2)
 
-        parsed_altitude_fractions = float(parsed_altitude_fractions)/16
+            data_fractions = np.binary_repr(-data_fractions, width = 4)
+            data_fractions = data_fractions[-4:]
+            data_fractions = -1*int(data_fractions, 2)
 
-        final_altitude = parsed_altitude + parsed_altitude_fractions
-        return final_altitude # meters
+        data_fractions = float(data_fractions)/16
 
-    def parse_raw_data_temp(raw_data):
-
-        str_msb = '{0:08b}'.format(raw_data[0])
-        str_lsb = '{0:08b}'.format(raw_data[1])
-
-        parsed_data = str_msb
-        parsed_data_fractions = str_lsb[0] + str_lsb[1] + str_lsb[2] + str_lsb[3]
-
-        parsed_temp = int(parsed_data, 2)
-        parsed_temp_fractions = int(parsed_data_fractions, 2)
-
-        if str_msb[0] == '1':
-            parsed_temp = np.binary_repr(-parsed_temp, width = 8)
-            parsed_temp = parsed_temp[-8:]
-            parsed_temp = -1*int(parsed_temp, 2)
-
-            parsed_temp_fractions = np.binary_repr(-parsed_temp_fractions, width = 4)
-            parsed_temp_fractions = parsed_temp_fractions[-4:]
-            parsed_temp_fractions = -1*int(parsed_temp_fractions, 2)
-
-        parsed_temp_fractions = float(parsed_temp_fractions)/16
-
-        final_temp = parsed_temp + parsed_temp_fractions
-        return final_temp # degrees C
+        final_data = data + data_fractions
+        return final_data # meters OR degrees C
