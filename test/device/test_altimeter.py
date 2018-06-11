bf7380814eabcc8e1d38634087150bc598ea7213
Anthony Lin <anthonyzqlin@gmail.com>
2018-06-11 14:46:07 -0400

    meh

--- test/device/test_altimeter.py
+++ test/device/test_altimeter.py
@@ -1,61 +1,50 @@
 import pytest
-from pytest import approx
-from app.device.altimeter import Altimeter
+import time
 
-def test_parse_raw_data_all_bits_1():
+from app.device import Altimeter
+from test.fixtures.dummy_device_factory import DummyDeviceFactory
+
+Altimeter = Altimeter()
+
+def test_altitude_1():
     raw_data = [255, 255, 255]
     expected_data = -1.0625
+    
+    rel_tol = 1e-3
+    assert Altimeter.parse_raw_data(raw_data) == approx(expected_data, rel=rel_tol)
 
-    parsed_data = Altimeter.parse_raw_data(raw_data)
+def test_altitude_0():
+    raw_data = [0, 0, 0]
+    expected_data = 0.0
 
     rel_tol = 1e-3
-    assert parsed_data == approx(expected_data, rel=rel_tol)
+    assert Altimeter.parse_raw_data(raw_data) == approx(expected_data, rel=rel_tol)
 
-def test_parse_raw_data_all_bits_0():
-    raw_data = [0,0,0]
-    expected_data = 0
+def test_altitude_rand():
+    raw_data = [255, 252, 160]
+    expected_data = 4.0625
 
-    parsed_data = Altimeter.parse_raw_data(raw_data)
+    rel_tol = 1e-3
+    assert Altimeter.parse_raw_data(raw_data) == approx(expected_data, rel=rel_tol)
 
-    rel_tol = 1e0
-    assert parsed_data == approx(expected_data, rel=rel_tol)
 
-def test_parse_raw_data_1km():
-    raw_data = [0,62,128]
-    expected_data = 1000
+def test_temp_level_1():
+    raw_data = [255, 255]
+    expected_data = 1.0625
 
-    parsed_data = Altimeter.parse_raw_data(raw_data)
+    rel_tol = 1e-3
+    assert Altimeter.parse_raw_data(raw_data) ==  approx(expected_data, rel=rel_tol)
 
-    rel_tol = 1e0
-    assert parsed_data == approx(expected_data, rel=rel_tol)
+def test_temp_level_0():
+    raw_data = [0, 0]
+    expected_data = 0.0
 
-def test_parse_raw_data_negative():
-    raw_data = [255,239,160]
-    expected_data = -262
+    rel_tol = 1e-3
+    assert Altimeter.parse_raw_data(raw_data) ==  approx(expected_data, rel=rel_tol)
 
-    parsed_data = Altimeter.parse_raw_data(raw_data)
-    rel_tol = 1e0
-    assert parsed_data == approx(expected_data, rel=rel_tol)
+def test_temp_level_rand():
+    raw_data = [23, 144]
+    expected_data = 23.4375
 
-def test_write_barometric_setting_negative():
-    alt = Altimeter()
-    new_setting = -1
-    old_setting = alt.read_bar_setting()
-    alt.write_bar_setting(new_setting)
-
-    new_setting = Altimeter.read_bar_setting()
-    assert new_setting == old_setting
-
-def test_write_barometric_setting_integer():
-    new_setting = 51727
-    alt.write_bar_setting(new_setting)
-
-    new_setting = alt.read_bar_setting()
-    assert new_setting == approx(new_setting, rel=1e0)
-
-def test_write_barometric_setting_decimal():
-    new_setting = 103454.17/2
-    Aalt.write_bar_setting(new_setting)
-
-    new_setting = alt.read_bar_setting()
-    assert new_setting == 103454
+    rel_tol = 1e-3
+    assert Altimeter.parse_raw_data(raw_data) ==  approx(expected_data, rel=rel_tol)
